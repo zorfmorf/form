7b6ab35c92e4d846d058697598347670fb1dd5af
NicNLD <nic-nld@live.nl>
2012-11-22 14:21:03 +0100

    Tagging 0.1.2

--- enable.php
+++ enable.php
@@ -11,7 +11,7 @@ if (!defined('IN_CMS')) { exit(); }
  * 
  * @author      Nic Wortel <nic.wortel@nth-root.nl>
  * @copyright   Nic Wortel, 2012
- * @version     0.1.1
+ * @version     0.1.2
  */
 
 Plugin::setAllSettings(array(
