0be0b9b312776f9c353f7a4eda227fca0f1d25e5
NicNLD <nic-nld@live.nl>
2013-09-05 21:54:41 +0200

    Added en-message.php

---
+++ i18n/en-message.php
@@ -0,0 +1,59 @@
+<?php
+if (!defined('IN_CMS')) { exit(); }
+
+/**
+ * Form
+ * 
+ * The Form plugin is a third-party plugin that lets you create and display forms on your installation of Wolf CMS.
+ * 
+ * @package     Plugins
+ * @subpackage  form
+ * 
+ * @author      Nic Wortel <nic.wortel@nth-root.nl>
+ * @copyright   Nic Wortel, 2012
+ * @version     0.1.2
+ */
+
+return array(
+    'Add form' => 'Add form',
+    'Add form field' => 'Add form field',
+    'Are you sure you wish to delete the form :name?' => 'Are you sure you wish to delete the form :name?',
+    'After enabling the Form plugin, you can click the Settings button to change the messages that are shown after submitting the form.' => 'After enabling the Form plugin, you can click the Settings button to change the messages that are shown after submitting the form.',
+    'Checkboxes' => 'Checkboxes',
+    'Delete' => 'Delete',
+    "Do you want a HTML5 compliant form? Use the following code:" => "Do you want a HTML5 compliant form? Use the following code:",
+    "Don't forget to replace id with the id of your form!" => "Don't forget to replace id with the id of your form!",
+    'Dropdown' => 'Dropdown',
+    'Edit form' => 'Edit form',
+    'Email address' => 'Email address',
+    'Email results to' => 'Email results to',
+    "Fill in the 'Form name' and 'Mail results to' fields, and create one or more form fields. Save the form." => "Fill in the 'Form name' and 'Mail results to' fields, and create one or more form fields. Save the form.",
+    'Form' => 'Form',
+    'Form has been saved!' => 'Form has been saved!',
+    'Form plugin' => 'Form plugin',
+    'Forms' => 'Forms',
+    'Form name' => 'Form name',
+    'How to use' => 'How to use',
+    'Humans should leave this field empty' => 'Humans should leave this field empty',
+    'ID' => 'ID',
+    'Label' => 'Label',
+    'Lets you create and display forms.' => 'Lets you create and display forms.',
+    'Long text' => 'Long text',
+    'Number' => 'Number',
+    'Options' => 'Options',
+    'Phone number' => 'Phone number',
+    'Postal code' => 'Postal code',
+    'Radio buttons' => 'Radio buttons',
+    'Required' => 'Required',
+    'Settings' => 'Settings',
+    'Submit' => 'Submit',
+    'Text' => 'Text',
+    'The Form plugin is a third-party plugin that lets you create and display forms on your installation of Wolf CMS.' => 'The Form plugin is a third-party plugin that lets you create and display forms on your installation of Wolf CMS.',
+    'To create a new form, click on the Add form button.' => 'To create a new form, click on the Add form button.',
+    'Type' => 'Type',
+    'Under options you can specify the different options in case of a dropdown list, checkboxes or radio buttons. Seperate with a semicolon.' => 'Under options you can specify the different options in case of a dropdown list, checkboxes or radio buttons. Seperate with a semicolon.',
+    'Use the following piece of PHP code in the page were you want to include the form:' => 'Use the following piece of PHP code in the page were you want to include the form:',
+    'You can seperate multiple e-mail addresses with a semicolon.' => 'You can seperate multiple e-mail addresses with a semicolon.',
+    'You have to specify a form name!' => 'You have to specify a form name!',
+    'You have to specify one or more valid email addresses!' => 'You have to specify one or more valid email addresses!'
+);
