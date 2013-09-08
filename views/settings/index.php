45716311749051f79b220d55345676f9a246a37a
NicNLD <nic-nld@live.nl>
2013-09-09 00:28:15 +0200

    Improved settings page

--- views/settings/index.php
+++ views/settings/index.php
@@ -22,20 +22,20 @@ if (!defined('IN_CMS')) { exit(); }
 <form method="post" action="<?php echo get_url('plugin/form/settings'); ?>">
     <table cellspacing="5" cellpadding="5" border="0">
         <tr>
-            <td><strong><?php echo __('Success message'); ?></strong></td>
-            <td><textarea name="setting[success_message]"><?php echo $settings['success_message']; ?></textarea></td>
+            <td><label for="setting_success_message"><?php echo __("'Success' message"); ?></label></td>
+            <td><textarea name="setting[success_message]" id="setting_success_message"><?php echo $settings['success_message']; ?></textarea></td>
         </tr>
         <tr>
-            <td><strong><?php echo __('Failure message'); ?></strong></td>
-            <td><textarea name="setting[invalid_message]"><?php echo $settings['invalid_message']; ?></textarea></td>
+            <td><label for="setting_invalid_message"><?php echo __("'Submission invalid' message"); ?></label></td>
+            <td><textarea name="setting[invalid_message]" id="setting_invalid_message"><?php echo $settings['invalid_message']; ?></textarea></td>
         </tr>
         <tr>
-            <td><strong><?php echo __('Error message'); ?></strong></td>
-            <td><textarea name="setting[error_message]"><?php echo $settings['error_message']; ?></textarea></td>
+            <td><label for="setting_error_message"><?php echo __("'Error' message"); ?></label></td>
+            <td><textarea name="setting[error_message]" id="setting_error_message"><?php echo $settings['error_message']; ?></textarea></td>
         </tr>
         <tr>
-            <td><strong><?php echo __('Copy message'); ?></strong></td>
-            <td><textarea name="setting[copy_message]"><?php echo $settings['copy_message']; ?></textarea></td>
+            <td><label for="setting_copy_message"><?php echo __("'Copy of submitted data' message"); ?></label></td>
+            <td><textarea name="setting[copy_message]" id="setting_copy_message"><?php echo $settings['copy_message']; ?></textarea></td>
         </tr>
         <tr>
             <td colspan="2">
