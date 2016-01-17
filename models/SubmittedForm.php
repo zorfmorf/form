ffd682690db7e1e36f77919eb1f1766fc120afd3
Sebastian Tauch <sebastian.tauch@aborium.com>
2016-01-17 15:48:04 +0100

    Overwrote submitted form method.

--- models/SubmittedForm.php
+++ models/SubmittedForm.php
@@ -21,55 +21,22 @@ class SubmittedForm
     
     public function save()
     {
-        use_helper('Email');
-                
-        $email_body = '';
-
-        $email_body .= "<table style=\"font: 12px/18px 'Arial','Helvetica',sans-serif;\">";
-        foreach($this->form->fields as $field) {
-            $value = $this->data[$field->slug];
-
-            if (is_array($value)) {
-                $value = implode(', ', $value);
-            }
-
-            $email_body .= '<tr><th style="text-align: left; vertical-align: top; white-space:nowrap;">'. $field->label .':</th><td style="text-align: left; vertical-align: top;">'.nl2br($value)."</td></tr>";
-            
-            if ($field->type == 'email_address') {
-                $sender = $this->data[$field->slug];
-            }
-        }
-        $email_body .= '</table>';
-
-        $email = new Email(array('mailtype' => 'html'));
-
-        $errors = 0;
-
-        $receivers = explode(';', $this->form->mail_to);
-        foreach ($receivers as $receiver) {
-            $email->from(Setting::get('admin_email'), Setting::get('admin_title'));
-            if (isset($sender) && !empty($sender)) {
-                $email->replyTo($sender);
-            }
-            $email->to(trim($receiver));
-            $email->subject($this->form->name);
-            $email->message($email_body);
-
-            if(!$email->send()) {
-                $errors++;
-            }
-        }
+		$errors = 0;
+		
+        $nr = $this->data['patientennummer'];
+        $name = $this->data['name'];
+        $email = $this->data['email'];
+        $anfrage = $this->data['anfrage'];
+        $text = $this->data['text'];
         
-        $copy_message = Plugin::getSetting('copy_message', 'form');
-        
-        if (isset($sender) && !empty($sender)) {
-            $email = new Email(array('mailtype' => 'html'));
-            $email->from(Setting::get('admin_email'), Setting::get('admin_title'));
-            $email->to($sender);
-            $email->subject($this->form->name);
-            $email->message($copy_message . $email_body);
-            $email->send();
-        }
+        if (isset($nr) && isset($name) && isset($email) && isset($anfrage) && isset($text))
+        {
+			save_new_patient_record($nr, $name, $email, $anfrage, $text);
+		}
+		else
+		{
+			$errors++;
+		}
         
         if ($errors > 0) {
             return false;
